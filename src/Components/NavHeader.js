7317c3853cc9e173f1e3816577603a1cb4916832
Sriram Paga <pagasriram@gmail.com>
2024-01-14 12:49:06 +0530

    Link for logo updated with /Home

--- src/Components/NavHeader.js
+++ src/Components/NavHeader.js
@@ -55,7 +55,7 @@ export default function Example() {
       >
         <div className="flex lg:flex-1">
           {/* <a href="/" className="-m-1.5 p-1.5"> */}
-          <Link to="/">
+          <Link to="/Home">
             <span className="sr-only">Your Company</span>
             <img className="h-16 w-auto" src="DilFoodsLogo.png" alt="" />
             {/* </a> */}
@@ -114,12 +114,11 @@ export default function Example() {
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <Link to='/'>
-
-            {/* <a href="/" className="m-1.5 p-1.5"> */}
+            <Link to="/Home">
+              {/* <a href="/" className="m-1.5 p-1.5"> */}
               <span className="sr-only">Your Company</span>
               <img className="h-8 w-auto" src="DilFoodsLogo.png" alt="" />
-            {/* </a> */}
+              {/* </a> */}
             </Link>
             <button
               type="button"
